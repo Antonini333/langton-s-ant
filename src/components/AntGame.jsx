2419583c5888193f9fedc54b58f39b27cfab1161
Antonini333 <pabloantoninivazquez@gmail.com>
2023-01-01 16:55:14 +0100

    Created grid object, nesting ant

--- src/components/AntGame.jsx
+++ src/components/AntGame.jsx
@@ -1,6 +1,26 @@
 const AntGame = () => {
+
+    const ANTUP = 0;
+    const ANTRIGHT = 1;
+    const ANTDOWN = 2;
+    const ANTLEFT = 3;
+
+    let grid = {
+        cells: [],
+        ant: {
+            x: 0,
+            y: 0,
+            direction: ANTUP
+        },
+        height: 0,
+        width: 0,
+        moves: 0
+    }
+
     return (
         <>
+        <canvas id="grid" width="500" height="500"></canvas>
+            <span id="moves"></span>
         </>
     )
 }
