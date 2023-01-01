4fbd7b534862f77913d525692c491469eb77f0e4
Antonini333 <pabloantoninivazquez@gmail.com>
2023-01-01 16:55:56 +0100

    Created function to start game

--- src/components/AntGame.jsx
+++ src/components/AntGame.jsx
@@ -17,6 +17,19 @@ const AntGame = () => {
         moves: 0
     }
 
+    const init = (grid) => {
+        if (!grid.cells.length) {
+            for (let x = 0; x < grid.width; x++) {
+                grid.cells[x] = [];
+                for (let y = 0; y < grid.height; y++) {
+                    let cell = { alive: false }
+                    grid.cells[x][y] = cell;
+                }
+            }
+            grid.ant = { ...grid.ant, x: grid.width / 2, y: grid.height / 2 }
+        }
+    }
+
     return (
         <>
         <canvas id="grid" width="500" height="500"></canvas>
